3224108333032f9a307a220ab3a965707580b35a
Jim Wharton <jamesawharton@gmail.com>
2013-07-26 08:32:33 -0600

    Add Craig Baker to the email distribution list

--- app/mailers/request_confirmation.rb
+++ app/mailers/request_confirmation.rb
@@ -1,10 +1,10 @@
 class RequestConfirmation < ActionMailer::Base
   def send_confirmation(request)
     @request = request
-    recipients = ['baerimagery@fs.fed.us', @request.requestor_email]
+    recipients = ['crbaker@fs.fed.us', 'baerimagery@fs.fed.us', @request.requestor_email]
     if @request.department == 'Dept of Interior'
       recipients << 'rmckinley@usgs.gov,jpicotte@usgs.gov,smhoward@usgs.gov'
     end
-      mail(:to => recipients.join(','), :subject => 'Your BAER Imagery/Mapping Request has been submitted', :from => 'baerimagery@fs.fed.us')      
-  end  
+      mail(:to => recipients.join(','), :subject => 'Your BAER Imagery/Mapping Request has been submitted', :from => 'baerimagery@fs.fed.us')
+  end
 end
